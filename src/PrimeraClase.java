dd13d48b893268b90e8ae277e4ad7a18ca88c254
PolloRosa <accarhuachagua@gmail.com>
2025-10-17 18:15:39 -0500

    Agrega ejercicio sobre operadores de incremento

--- src/PrimeraClase.java
+++ src/PrimeraClase.java
@@ -10,5 +10,22 @@ public class PrimeraClase {
         double media =  (8.2 + 6.0 + 9.0) / 3;
 
         System.out.println(media);
+
+        // ejercicio sobre operadores de incremento
+        // Operador de pre-incremento
+        // primero aumenta el valor, después la utiliza en la expresión
+        int puntos = 10;
+        int puntosVip = ++puntos;
+        System.out.println("Los puntos son " + puntos + " y para VIP son " + puntosVip);
+        // Operador de post-incremento
+        // utiliza la variable en la expresión, después aumenta su valor
+        puntosVip = puntos++;
+        System.out.println("Los puntos son " + puntos + " y para VIP son " + puntosVip);
+
+        if(puntosVip == puntos++) {
+            System.out.println("Primero comprueba la condición, después incrementa a "+ puntos + ".");
+        } else {
+            System.out.println("Primero incrementa a " + puntos + ", después comprueba la condición.");
+        }
     }
 }
