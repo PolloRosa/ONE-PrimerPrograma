873667fd0e520c88564b4b2992cc9d80832bb23f
PolloRosa <accarhuachagua@gmail.com>
2025-10-19 17:48:54 -0500

    Corrige y ordena código según sugerencias del instructor en clase Ejercicio

--- src/Ejercicio.java
+++ src/Ejercicio.java
@@ -1,14 +1,14 @@
 public class Ejercicio {
     public static void main(String[] args) {
         double temperaturaCelsius = 25.5;
+        double temperaturaFahrenheit = (temperaturaCelsius * 1.8) + 32;
         int temperaturaEntera;
 
-        double temperaturaFahrenheit = (temperaturaCelsius * 1.8) + 32;
-
         System.out.println("Conversión de temperaturas exitosa.");
-        System.out.printf("La temperatura %.2f grados Celsius equivale a %.2f grados Fahrenheit.%n", temperaturaCelsius, temperaturaFahrenheit);
+        System.out.printf("La temperatura %f grados Celsius equivale a %f grados Fahrenheit.%n", temperaturaCelsius, temperaturaFahrenheit);
 
         temperaturaEntera = (int) temperaturaFahrenheit;
-        System.out.printf("La temperatura en grados Fahrenheit entera es %d", temperaturaEntera);
+        String imprimir = String.format("La temperatura en grados Fahrenheit entera es %d.", temperaturaEntera);
+        System.out.println(imprimir);
     }
 }
