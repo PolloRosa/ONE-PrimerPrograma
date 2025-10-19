470212b48f073e85e8b63190b07f2fdede0911f5
PolloRosa <accarhuachagua@gmail.com>
2025-10-19 17:19:08 -0500

    Agrega ejercicio sobre formateo de textos

--- src/PrimeraClase.java
+++ src/PrimeraClase.java
@@ -7,9 +7,8 @@ public class PrimeraClase {
         boolean incluidoEnElPlan = true;
         double notaDeLaPelicula = 8.2;
 
-        double media =  (8.2 + 6.0 + 9.0) / 3;
-
-        System.out.println(media);
+        /*double media =  (8.2 + 6.0 + 9.0) / 3;
+        System.out.println(media);*/
 
         String sinopsis = """
                 Matrix es una paradoja.
@@ -17,5 +16,16 @@ public class PrimeraClase {
                 Fue lanzada en: 
                 """ + fechaDeLanzamiento;
         System.out.println(sinopsis);
+
+        String nombre = "Angela", signo = "Piscis", servicio = "Uber", destino = "Tecsup Santa Anita";
+        int edad = 37;
+        double tarifa = 14.456;
+        System.out.println(String.format("Le saluda %s, le escribo por el aviso.", nombre));
+        System.out.printf("Hola, mi nombre es %s, tengo %d años, soy del signo %s. Mucho gusto.%n", nombre, edad, signo);
+        String cadenaSalida = """
+                Su último viaje en %s fue a %s, su precio fue de %.2f.
+                Confirmar para reagendar. (S/N)
+                """.formatted(servicio, destino, tarifa);
+        System.out.println(cadenaSalida);
     }
 }
